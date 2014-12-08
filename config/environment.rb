05568b09cbe9fbb44b4396cbae7d2e84087337ec
jesuisuncaillou <igorlacroute@gmail.com>
2014-12-08 16:16:49 +0100

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+EcomicsV01::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EcomicsV01::Application.config.secret_key_base = 'f35cb41ab665e6395bad6fc0b02588823fa4285f61c75368470c19ed7307bde215b94f3ff80fd67981c5fe65f41a0f302cf2ceb9e4d483bf24edc2f1e2bf3761'
