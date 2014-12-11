8ed962cc733393a1939440a9cae306484696f255
jesuisuncaillou <igorlacroute@gmail.com>
2014-12-11 17:27:48 +0100

    init des specs requests ecomics

--- spec/requests/ecomic_pages_spec.rb
+++ spec/requests/ecomic_pages_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
 describe "eComic pages" do
+  
+  subject { page }
 
+  describe "New eComic Page" do
+    before { visit new_ecomic_path }
+
+    it { should have_content("New ecomic") }
+  end
 
 
 end
