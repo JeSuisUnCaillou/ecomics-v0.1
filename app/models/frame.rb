31e75fc20b016459a1df85dbc815915dbc54511a
jesuisuncaillou <igorlacroute@gmail.com>
2015-01-10 14:54:10 +0100

    hide aws keys

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -9,8 +9,7 @@ class Frame < ActiveRecord::Base
   has_attached_file :picture,
                      :styles => { :medium => "300x300>", :thumb => "100x100>" },
                      :default_url => "/images/:style/missing.png",
-                     :storage => :s3,
-                     :bucket => "mypopcomics-bucket"
+                     :storage => :s3
 
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,10 @@ EcomicsV01::Application.configure do
     :storage => :s3,
     :s3_host_name => 's3.eu-central-1.amazonaws.com',
     :s3_credentials => {
-      :bucket => 'mypopcomics-bucket'
+     # :bucket => 'mypopcomics-bucket'
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 
