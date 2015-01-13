9e5bd21b7115ec8561e51072431ee08028936cc8
jesuisuncaillou <igorlacroute@gmail.com>
2015-01-13 19:24:19 +0100

    config pour amazon s3 dans l'env test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,15 @@ EcomicsV01::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_host_name => 's3.eu-central-1.amazonaws.com',
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+
+    }
+  }
 end
